d6d996923d809dd968f7b74481f692a111c5efd9
Heru Handika <herubiolog@gmail.com>
2022-09-22 15:43:49 -0500

    Add locality and grid to the records.

--- src/handler/organizer.rs
+++ src/handler/organizer.rs
@@ -28,48 +28,54 @@ impl Organizer {
     pub fn organize(&mut self, input_dir: &Path, cfg_path: &Path, output_dir: &Path) {
         let img_paths = Finder::new(input_dir).scan_directory();
         self.parse_config_csv(cfg_path);
-        self.parse_img_records(&img_paths);
+        self.parse_img_records();
         self.print_input(input_dir, &cfg_path);
-        self.organize_by_taxa(output_dir);
+        self.organize_by_taxa(&img_paths, output_dir);
     }
 
-    fn organize_by_taxa(&self, output_dir: &Path) {
+    fn organize_by_taxa(&self, img_paths: &[PathBuf], output_dir: &Path) {
         let spin = spinner::set_spinner();
         spin.set_message("Organizing images by taxa");
-        self.records.iter().for_each(|rec| {
-            let taxon_name = rec.scientific_id.trim().replace(" ", "_").replace(".", "");
-            match self.img_records.get(&rec.image_id) {
-                Some(img_path) => {
-                    let taxon_path = Path::new(&taxon_name);
-                    let output_path = output_dir.join(taxon_path).join(img_path);
-                    fs::create_dir_all(output_path.parent().expect("Could not get parent path"))
-                        .expect("Could not create directory");
-                    match fs::rename(img_path, output_path) {
-                        Ok(_) => (),
-                        Err(e) => {
-                            spin.set_message(format!("Could not move image: {}", e));
-                            return;
-                        }
-                    }
-                }
-                None => {
-                    log::error!("Could not find image path for {}", rec.image_id);
-                    return;
-                }
-            };
-        });
-        spin.finish_with_message("Done organizing images by taxa!");
-    }
 
-    fn parse_img_records(&mut self, img_paths: &[PathBuf]) {
         img_paths.iter().for_each(|img_path| {
             let img_name = img_path
                 .file_name()
-                .expect("Error parsing image name")
+                .expect("Failed parsing filenames")
                 .to_string_lossy()
                 .to_uppercase()
                 .to_string();
-            self.img_records.insert(img_name, img_path.clone());
+
+            let output = match self.img_records.get(&img_name) {
+                Some(path) => Path::new(path),
+                None => Path::new("unknown"),
+            };
+
+            let output_path = output_dir.join(output).join(img_name);
+            fs::create_dir_all(output_path.parent().expect("Could not get parent path"))
+                .expect("Could not create directory");
+            match fs::rename(img_path, &output_path) {
+                Ok(_) => (),
+                Err(e) => {
+                    spin.set_message(format!(
+                        "Could not move image: {} for {}",
+                        e,
+                        output_path.display()
+                    ));
+                    return;
+                }
+            }
+        });
+
+        spin.finish_with_message("Done organizing images by taxa!");
+    }
+
+    fn parse_img_records(&mut self) {
+        self.records.iter().for_each(|rec| {
+            let taxon_name = rec.scientific_id.trim().replace(" ", "_").replace(".", "");
+            let path = Path::new(&taxon_name)
+                .join(&rec.locality)
+                .join(&rec.station);
+            self.img_records.insert(rec.image_id.to_uppercase(), path);
         })
     }
 
@@ -94,6 +100,8 @@ impl Organizer {
 
 #[derive(Debug, Deserialize)]
 struct ImgRecords {
+    locality: String,
+    station: String,
     image_id: String,
     scientific_id: String,
 }
